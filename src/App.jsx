bd34a169739692e712af0b2dcedd9001e53d22ca
Anam0702Khan <129427654+Anam0702Khan@users.noreply.github.com>
2024-08-23 11:15:19 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -8,8 +8,10 @@ function App() {
 
   return (
     <>
-      <div className='bg-white w-1/2 mx-auto rounded-lg mb-8'>
-       <h1 className="text-black font-bold text-xl mt-12">To-do List<FaCalendarAlt className="inline-block text-black-500 ml-2" /></h1>
+      <div className='bg-white w-1/2 mx-auto rounded-lg mb-8 mt-8'>
+      <div className='mt-3 pt-3'>
+       <h1 className="text-black font-bold text-2xl mt-12">To-do List<FaCalendarAlt className="inline-block text-black-500 ml-2 mb-1" /></h1>
+       </div>
       <AddTask />
       <TodoList />
       </div>
